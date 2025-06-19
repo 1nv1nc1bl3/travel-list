7f673675df4ebd9340b183bffcfb6f1fd3e8d698
Alkis Karagiannis <alkis.k1972@gmail.com>
2025-06-19 09:49:12 +0300

    start of Day 4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import Logo from './Logo';
+import Stats from './Stats';
+
 const initialItems = [
     { id: 1, description: 'Passports', quantity: 2, packed: false },
     { id: 2, description: 'Socks', quantity: 12, packed: true },
@@ -14,10 +17,6 @@ export default function App() {
             <Stats />
         </div>
     );
-}
-
-function Logo() {
-    return <h1>🌴 Far Away 💼</h1>;
 }
 
 function Form() {
@@ -65,15 +64,5 @@ function Item({ packingItem }) {
             </span>
             <button>❌</button>
         </li>
-    );
-}
-
-function Stats() {
-    return (
-        <footer className='stats'>
-            <em>
-                💼 You have X items on your list and you already packed X (X%)
-            </em>
-        </footer>
     );
 }
