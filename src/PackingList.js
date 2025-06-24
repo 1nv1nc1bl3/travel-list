5dbcce8327b356098c951525734726a8acad1756
Alkis Karagiannis <alkis.k1972@gmail.com>
2025-06-24 19:14:50 +0300

    Modified PackingList.js

--- src/PackingList.js
+++ src/PackingList.js
@@ -12,6 +12,13 @@ export default function PackingList({ items, onDeleteItem, onToggleItem }) {
                     />
                 ))}
             </ul>
+            <div className='actions'>
+                <select>
+                    <option value='input'>Sort by input order</option>
+                    <option value='description'>Sort by description</option>
+                    <option value='packed'>Sort by packed status</option>
+                </select>
+            </div>
         </div>
     );
 }
