07b8f47e389d92a5ba6e689ee78c115f245e8182
Alkis <alkis.k1972@gmail.com>
2025-10-05 14:22:46 +0300

    modified Stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -11,7 +11,7 @@ function Stats({ numItems, numPacked, percentage }) {
                 <em>
                     {percentage === 100
                         ? 'You got everything! Ready to go'
-                        : `You have ${numItems} items on your list and you already packed
+                        : `You have ${numItems} item${numItems === 1 ? '' : 's'} on your list and you already packed
                 ${numPacked} (${percentage}%)`}
                 </em>
             </footer>
